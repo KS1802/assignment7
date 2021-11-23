e1d9a671b0505b6c36570e93d9190886326164c1
Bluerent <93621495+Bluerent@users.noreply.github.com>
2021-11-23 15:17:15 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,75 +1,204 @@
+
 (function() {
-    "use strict";
-// List of sentences
-var _CONTENT = [ 
-	"Twinkle, twinkle, little star", 
-	"How I wonder what you are", 
-	"Up above the world so high", 
-	"Like a diamond in the sky"
-];
-
-// Current sentence being processed
-var _PART = 0;
-
-// Character number of the current sentence being processed 
-var _PART_INDEX = 0;
-
-// Holds the handle returned from setInterval
-var _INTERVAL_VAL;
-
-// Element that holds the text
-var _ELEMENT = document.querySelector("#text");
-
-// Cursor element 
-var _CURSOR = document.querySelector("#cursor");
-
-// Implements typing effect
-function Type() { 
-	// Get substring with 1 characater added
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX++;
-
-	// If full sentence has been displayed then start to delete the sentence after some time
-	if(text === _CONTENT[_PART]) {
-		// Hide the cursor
-		_CURSOR.style.display = 'none';
-
-		clearInterval(_INTERVAL_VAL);
-		setTimeout(function() {
-			_INTERVAL_VAL = setInterval(Delete, 50);
-		}, 1000);
+	"use strict";
+  
+	/**
+	 * Easy selector helper function
+	 */
+	const select = (el, all = false) => {
+	  el = el.trim()
+	  if (all) {
+		return [...document.querySelectorAll(el)]
+	  } else {
+		return document.querySelector(el)
+	  }
 	}
-}
-
-// Implements deleting effect
-function Delete() {
-	// Get substring with 1 characater deleted
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX--;
-
-	// If sentence has been deleted then start to display the next sentence
-	if(text === '') {
-		clearInterval(_INTERVAL_VAL);
-
-		// If current sentence was last then display the first one, else move to the next
-		if(_PART == (_CONTENT.length - 1))
-			_PART = 0;
-		else
-			_PART++;
-		
-		_PART_INDEX = 0;
-
-		// Start to display the next sentence after some time
-		setTimeout(function() {
-			_CURSOR.style.display = 'inline-block';
-			_INTERVAL_VAL = setInterval(Type, 100);
-		}, 200);
+  
+	/**
+	 * Easy event listener function
+	 */
+	const on = (type, el, listener, all = false) => {
+	  let selectEl = select(el, all)
+	  if (selectEl) {
+		if (all) {
+		  selectEl.forEach(e => e.addEventListener(type, listener))
+		} else {
+		  selectEl.addEventListener(type, listener)
+		}
+	  }
 	}
-}
-
-// Start the typing effect on load
-_INTERVAL_VAL = setInterval(Type, 100);
-}
-())
+  
+	/**
+	 * Easy on scroll event listener 
+	 */
+	const onscroll = (el, listener) => {
+	  el.addEventListener('scroll', listener)
+	}
+  
+	/**
+	 * Navbar links active state on scroll
+	 */
+	let navbarlinks = select('#navbar .scrollto', true)
+	const navbarlinksActive = () => {
+	  let position = window.scrollY + 200
+	  navbarlinks.forEach(navbarlink => {
+		if (!navbarlink.hash) return
+		let section = select(navbarlink.hash)
+		if (!section) return
+		if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+		  navbarlink.classList.add('active')
+		} else {
+		  navbarlink.classList.remove('active')
+		}
+	  })
+	}
+	window.addEventListener('load', navbarlinksActive)
+	onscroll(document, navbarlinksActive)
+  
+	/**
+	 * Scrolls to an element with header offset
+	 */
+	const scrollto = (el) => {
+	  let header = select('#header')
+	  let offset = header.offsetHeight
+  
+	  if (!header.classList.contains('header-scrolled')) {
+		offset -= 16
+	  }
+  
+	  let elementPos = select(el).offsetTop
+	  window.scrollTo({
+		top: elementPos - offset,
+		behavior: 'smooth'
+	  })
+	}
+  
+	/**
+	 * Toggle .header-scrolled class to #header when page is scrolled
+	 */
+	let selectHeader = select('#header')
+	if (selectHeader) {
+	  const headerScrolled = () => {
+		if (window.scrollY > 100) {
+		  selectHeader.classList.add('header-scrolled')
+		} else {
+		  selectHeader.classList.remove('header-scrolled')
+		}
+	  }
+	  window.addEventListener('load', headerScrolled)
+	  onscroll(document, headerScrolled)
+	}
+  
+	/**
+	 * Back to top button
+	 */
+	let backtotop = select('.back-to-top')
+	if (backtotop) {
+	  const toggleBacktotop = () => {
+		if (window.scrollY > 100) {
+		  backtotop.classList.add('active')
+		} else {
+		  backtotop.classList.remove('active')
+		}
+	  }
+	  window.addEventListener('load', toggleBacktotop)
+	  onscroll(document, toggleBacktotop)
+	}
+  
+	/**
+	 * Mobile nav toggle
+	 */
+	on('click', '.mobile-nav-toggle', function(e) {
+	  select('#navbar').classList.toggle('navbar-mobile')
+	  this.classList.toggle('bi-list')
+	  this.classList.toggle('bi-x')
+	})
+  
+	/**
+	 * Mobile nav dropdowns activate
+	 */
+	on('click', '.navbar .dropdown > a', function(e) {
+	  if (select('#navbar').classList.contains('navbar-mobile')) {
+		e.preventDefault()
+		this.nextElementSibling.classList.toggle('dropdown-active')
+	  }
+	}, true)
+  
+	/**
+	 * Scrool with ofset on links with a class name .scrollto
+	 */
+	on('click', '.scrollto', function(e) {
+	  if (select(this.hash)) {
+		e.preventDefault()
+  
+		let navbar = select('#navbar')
+		if (navbar.classList.contains('navbar-mobile')) {
+		  navbar.classList.remove('navbar-mobile')
+		  let navbarToggle = select('.mobile-nav-toggle')
+		  navbarToggle.classList.toggle('bi-list')
+		  navbarToggle.classList.toggle('bi-x')
+		}
+		scrollto(this.hash)
+	  }
+	}, true)
+  
+	/**
+	 * Scroll with ofset on page load with hash links in the url
+	 */
+	window.addEventListener('load', () => {
+	  if (window.location.hash) {
+		if (select(window.location.hash)) {
+		  scrollto(window.location.hash)
+		}
+	  }
+	});
+  
+	/**
+	 * Intro type effect
+	 */
+	const typed = select('.typed')
+	if (typed) {
+	  let typed_strings = typed.getAttribute('data-typed-items')
+	  typed_strings = typed_strings.split(',')
+	  new Typed('.typed', {
+		strings: typed_strings,
+		loop: true,
+		typeSpeed: 100,
+		backSpeed: 50,
+		backDelay: 2000
+	  });
+	}
+  
+   
+	/**
+	 * Preloader
+	 */
+	let preloader = select('#preloader');
+	if (preloader) {
+	  window.addEventListener('load', () => {
+		preloader.remove()
+	  });
+	}
+  
+  })()
+  function openCity(evt, cityName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+	
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+	
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";}
+  
+	
